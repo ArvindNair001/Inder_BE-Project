513d13ed0f00b16f7f0bc83fff95d7749d2d57de
Arvind Hariharan Nair <arvdev96@gmail.com>
2019-04-25 22:20:14 +0530

    Backend Fix

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 from flask import Flask
-import flask
-import routes
+# from routes import *
+
+
 app = Flask(__name__, static_url_path='/static')
 
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+from routes import * 
 
-from routes import *;
+# @app.route('/')
+# def index():
+#     return 'INDEX'
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT,debug=True)
-
-'''
-    debug = True will ensure that you dont have to restart the server after 
-    every change.. Thank me later ;-) DCOMFORT...
-'''
+    app.run(debug=True)
--- routes.py
+++ routes.py
@@ -1,11 +1,43 @@
 import flask
-from flask import Flask, url_for, render_template;
+from flask import Flask, url_for, request, render_template,jsonify;
+from werkzeug import secure_filename
+import os
 from app import app;
+
+# print(os.getcwd())
+UPLOAD_FOLDER = os.path.join(os.getcwd(),'Files')
+# print(UPLOAD_FOLDER)
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+    print('not exists')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('home.html');
 
-@app.route('/process',methods=["POST"])
+@app.route('/process',methods=['POST'])
 def execute():
-    return '';
+    if request.method == 'POST':
+        data = request.form['inputdata']
+        return jsonify({'data': data})
+    return jsonify({'error': 'Invalid method'})
+    
+@app.route('/upload',methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        print(filename)
+        # file.save(secure_filename(file.filename))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return ''
+        
+@app.route('/jsontest',methods=['POST'])
+def jsontest():
+    data = request.get_json()
+    name = data['name']
+    age = data['age']
+    return jsonify({'success': 'ok', 'name': name, 'age': age})
